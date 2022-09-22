a077ab5c1151ad6013d8df22ff3e2926e25f947b
Rolando-Barbella <rolandobarbella@gmail.com>
2022-09-22 22:29:46 +0100

    Fix typo

--- checkout.ts
+++ checkout.ts
@@ -14,7 +14,7 @@ export async function checkout({lineItems}) {
     await stripe.redirectToCheckout({
       mode: 'payment',
       lineItems,
-      succesUrl: `${window.location.origin}?session_id={CHECKOUT_SESSION_ID}`,
+      successUrl: `${window.location.origin}?session_id={CHECKOUT_SESSION_ID}`,
       cancelUrl: window.location.origin,
     })
 }
