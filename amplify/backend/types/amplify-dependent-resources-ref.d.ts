75b82e143f7b7fed9e243a50b66e835641ce1ccb
Rolando-Barbella <rolandobarbella@gmail.com>
2022-10-08 23:49:52 +0100

    Add files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "trabajodevb1da41d5": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "trabajoProjct": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "trabajodev": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
