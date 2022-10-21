79008efdd6a0422f54356fb274b5e275170a8be6
Rolando-Barbella <rolandobarbella@gmail.com>
2022-10-21 22:12:35 +0100

    Tidy condition

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import { AmplifyS3Image } from "@aws-amplify/ui-react/legacy";
 import { withSSRContext } from "aws-amplify";
-import { Auth } from 'aws-amplify';
+import { Auth } from "aws-amplify";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
@@ -21,26 +21,26 @@ export async function getServerSideProps() {
 //@ts-ignorets
 const Home: NextPage = ({ companies }) => {
   console.log(process.env.NODE_ENV);
-  const Router =  useRouter()
-  let [currentUser, setCurrentUser] = React.useState("")
-  
+  const Router = useRouter();
+  let [currentUser, setCurrentUser] = React.useState("");
+
   React.useEffect(() => {
     let getUser = async () => {
       try {
         let user = await Auth.currentAuthenticatedUser();
         setCurrentUser(user);
-      } catch(e) {
-        console.error(e)
+      } catch (e) {
+        console.error(e);
       }
-    }
+    };
     getUser();
-  },[])
+  }, []);
 
   let signOut = (e: any) => {
-    e.preventDefault()
-    Auth.signOut()
-    Router.reload()
-  }
+    e.preventDefault();
+    Auth.signOut();
+    Router.reload();
+  };
   return (
     <div className={styles.container}>
       <div>
@@ -48,35 +48,35 @@ const Home: NextPage = ({ companies }) => {
           <title>Companies hiring</title>
         </Head>
         <div className="container">
-          {
-            Boolean(currentUser) && (
-              <>
-                <Link href="/profile">Profile</Link>
-                <Link href="/profile" onClick={signOut}>Sig Out</Link>
-              </>
-            )
-          }
-          <h1>
-            {
-              process.env.NODE_ENV === 'development' ? (
-                <>
-                  National Companies <Link href="/create-company">(+)</Link>
-                </>
-              ) : ( <>Junior dev Jobs</>)
-            }
-          </h1>
-          <div className="img-grid">
-            {/* @ts-ignorets */}
-            {companies?.map((company) => {
-              return (
-                <div key={company.id} className="img-square">
-                  <h2>{company.name}</h2>
-                  {/* use the AmplifyS3Image component to render the company's image using its S3 key */}
-                  <AmplifyS3Image imgKey={company.image.key} />
-                </div>
-              );
-            })}
-          </div>
+          {Boolean(currentUser) && (
+            <>
+              <Link href="/profile">Profile</Link>
+              <Link href="/profile" onClick={signOut}>
+                Sig Out
+              </Link>
+            </>
+          )}
+          {process.env.NODE_ENV === "development" ? (
+            <>
+              <h1>
+                National Companies <Link href="/create-company">(+)</Link>
+              </h1>
+              <div className="img-grid">
+                {/* @ts-ignorets */}
+                {companies?.map((company) => {
+                  return (
+                    <div key={company.id} className="img-square">
+                      <h2>{company.name}</h2>
+                      {/* use the AmplifyS3Image component to render the company's image using its S3 key */}
+                      <AmplifyS3Image imgKey={company.image.key} />
+                    </div>
+                  );
+                })}
+              </div>
+            </>
+          ) : (
+            <><h1>Junior dev Jobs</h1></>
+          )}
         </div>
       </div>
     </div>
