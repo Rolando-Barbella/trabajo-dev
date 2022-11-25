fb1516581eab9660b55c5d31cd29060e7c58fc9f
Rolando-Barbella <rolandobarbella@gmail.com>
2022-11-25 12:45:08 -0400

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,23 @@ import Amplify from 'aws-amplify'
 import config from '../src/aws-exports'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 Amplify.configure({ ...config, ssr: true })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+          html {
+            font-family: ${inter.style.fontFamily};
+          } 
+        `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
