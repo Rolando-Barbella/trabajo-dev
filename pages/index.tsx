904ceaac1382c3be5d616da26cb8a9e6fc7b4352
Rolando-Barbella <rolandobarbella@gmail.com>
2023-01-11 00:44:48 -0400

    No found job scenario added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
+import Image from 'next/image'
 import { withSSRContext } from "aws-amplify";
 import Head from "next/head";
 import { listJobs } from "../src/graphql/queries";
+import Box from '@mui/material/Box';
 import JobCard from "../src/components/JobCard/JobCard";
 import { Job } from "../src/API";
+import { CustomButton as Button } from "../src/components/CustomButton/CustomButton";
+import Link from "next/link";
+
 
 export async function getServerSideProps() {
   const SSR = withSSRContext();
@@ -16,13 +20,36 @@ export async function getServerSideProps() {
   }
   return {
     props: {
-      jobs: data?.listJobs?.items,
+      jobs: data?.listJobs?.items.filter((job: Job) => Boolean(job.hasbeenPaid)) || null,
     },
   };
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    flex: 1,
+    minHeight: '100vh',
+  },
+  noJobs: {
+    display: 'flex',
+    flexDirection: 'column', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    minHeight: '100vh', 
+    alignSelf: 'center'
+  },
+  noJobsMessage: {
+    display: 'flex',
+    flexDirection: 'column', 
+    textAlign: 'center',
+    alignItems: 'center', 
+    alignSelf: 'center'
+  }
+}
 const Home = ({ jobs }: {jobs: Array<Job>}) => {
   return (
-    <>
+    <Box sx={styles.container}>
       <Head>
         <title>Software development jobs for junior</title>
       </Head>
@@ -38,9 +65,19 @@ const Home = ({ jobs }: {jobs: Array<Job>}) => {
             </div>
           )}
         </h1>
-        <div>
+        <>
           {
-            !jobs?.length && <>No jobs</>
+            !jobs?.length ? (
+              <Box style={styles.noJobs}>
+                <div style={styles.noJobsMessage}>
+                  <Image alt="sad face" src="/sad-face.png" width={400} height={400}/>
+                  <h1 className="font-light text-2xl pb-4">Nothing here, help us get jobs for junior devs! </h1>
+                  <Link href="/create-job">
+                    <Button text="Post a job" width={120} />
+                  </Link>
+                </div>
+              </Box>
+            ): null
           }
           {jobs?.map((job: Job) => {
             return (
@@ -49,9 +86,9 @@ const Home = ({ jobs }: {jobs: Array<Job>}) => {
               </div>
             );
           })}
-        </div>
+        </>
       </div>
-    </>
+    </Box>
   );
 };
 
