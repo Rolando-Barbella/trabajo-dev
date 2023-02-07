decfc834627b8b70ecda68db9c3709f6f7853fc1
Rolando-Barbella <rolandobarbella@gmail.com>
2023-02-06 21:36:49 -0400

    end point

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -26,7 +26,7 @@ const awsmobile = {
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
     ],
-    "aws_appsync_graphqlEndpoint": "https://knray2auxbe5nprztkbsgn2uca.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://vgvds3l4e5d4rcik2r44s4s4sa.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
     "aws_appsync_apiKey": "da2-fakeApiId123456",
