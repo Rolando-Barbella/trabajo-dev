2de5e13e5de8ec6bad751348403c9eae54a17481
Rolando-Barbella <rolandobarbella@gmail.com>
2023-02-11 21:19:55 -0400

    .

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -3,11 +3,11 @@ import Container from "@mui/material/Container";
 
 function Contact() {
   return (
-    <Container maxWidth="md" sx={{ pt: 2, pb: 37 }}>
-      <div className="text-center grid place-items-center pt-10">
+    <Container maxWidth="md" sx={{ pt: 2, pb: 50 }}>
+      <div className="text-center grid place-items-center pt-10 text-lg text-light">
         <div>
-          <p className="text-light text-lg">We are a small team ( nah, is just one person ) helping junior dev get the right oportunities.</p>
-          <div className="pt-2 text-lg">
+          <p>We are a small team ( nah, is just one person ) helping junior dev get the right oportunities.</p>
+          <div className="pt-2">
             <p>Any questions or queries, please feel free to text at:</p>
             <p className="text-blue-400">juniordevelopersjobs@gmail.com</p>
           </div>
